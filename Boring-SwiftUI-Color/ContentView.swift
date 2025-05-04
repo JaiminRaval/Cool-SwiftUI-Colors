fa1d62df425f807f40f969afd5904175f580498a
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-05-04 19:20:58 +0530

    added simple playful UI

--- Boring-SwiftUI-Color/ContentView.swift
+++ Boring-SwiftUI-Color/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorVal:Double = 0.5
     var body: some View {
         VStack {
             RoundedRectangle(cornerRadius: 12, style: .continuous)
                 .fill(Color.red)
                 .frame(height: 100)
+            
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .fill(Color.red)
+                .frame(height: 100)
+            
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .fill(Color.red)
+                .frame(height: 100)
+            
+            Spacer()
+            Slider(value: $colorVal, in: 0...1)
+            Spacer()
         }
         .padding()
     }
