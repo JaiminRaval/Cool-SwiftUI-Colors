e273cffbf939bb0d66f8f4c2f2e394261daaf5f3
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-05-04 22:18:43 +0530

    Update ContentView.swift

--- Boring-SwiftUI-Color/ContentView.swift
+++ Boring-SwiftUI-Color/ContentView.swift
@@ -11,20 +11,23 @@ struct ContentView: View {
     @State private var colorVal:Double = 0.5
     var body: some View {
         VStack {
+            Spacer()
             RoundedRectangle(cornerRadius: 12, style: .continuous)
-                .fill(Color.red)
+                .fill(Color.cyan)
+                .grayscale(colorVal)
                 .frame(height: 100)
             
             RoundedRectangle(cornerRadius: 12, style: .continuous)
-                .fill(Color.red)
+                .fill(Color.cyan)
                 .frame(height: 100)
             
             RoundedRectangle(cornerRadius: 12, style: .continuous)
-                .fill(Color.red)
+                .fill(Color.cyan)
                 .frame(height: 100)
             
             Spacer()
             Slider(value: $colorVal, in: 0...1)
+            Spacer()
             Spacer()
         }
         .padding()
