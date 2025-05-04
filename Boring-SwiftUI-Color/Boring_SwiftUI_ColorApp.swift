2c90830907283f7a935727048c41ff1b09d837da
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-05-04 18:44:31 +0530

    Initial Commit

---
+++ Boring-SwiftUI-Color/Boring_SwiftUI_ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boring_SwiftUI_ColorApp.swift
+//  Boring-SwiftUI-Color
+//
+//  Created by Jaimin Raval on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Boring_SwiftUI_ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
